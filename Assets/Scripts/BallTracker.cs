232b50e62ecbfa1c302826a5065672de6bc68102
dairexwolf <93929534+dairexwolf@users.noreply.github.com>
2023-09-04 16:59:34 +0700

    Add ball tracker scripts

--- Assets/Scripts/BallTracker.cs
+++ Assets/Scripts/BallTracker.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class BallTracker : MonoBehaviour
 {
+
+    GameManager _gm;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        if (_gm != null)
+        {
+            Debug.Log("GM found");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ball") && _gm != null)
+        {
+            float _yVelocity = other.GetComponent<Rigidbody>().velocity.y;
+            if (_yVelocity < -0.1)
+            {
+                Debug.Log("Ball Y velocity: " + _yVelocity);
+                _gm.Count++;
+                _gm.ChangeVisibleCounter();
+            }
+        }
+
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    int count;
+
+    [SerializeField]
+    TextMeshPro _text;
+
+    public int Count
+    {
+        get
+        {
+            return count;
+        }
+        set
+        {
+            count = value;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void ChangeVisibleCounter()
+    {
+        _text.text = count.ToString();
     }
 }
